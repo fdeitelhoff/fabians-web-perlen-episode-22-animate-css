532403ccc5688aac1d7a0ff15ac30c794e5e3124
fdeitelhoff <fabian@fabiandeitelhoff.de>
2022-10-07 17:12:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fabians-web-perlen-episode-22-animate-css
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/fabians-web-perlen-episode-22-animate-css)
---
+++ index.js
@@ -0,0 +1,8 @@
+// Import Animate.css styles.
+import 'animate.css';
+
+import './index.css';
+
+// Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
